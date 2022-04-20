62bbb40babd7593975b7b05158147a598284fad1
RichardwindF <romewind1@gmail.com>
2022-04-20 13:41:57 -0400

    Jetpack Compose standard layout component

---
+++ JetpackComposeLayouts/src/main/java/com/example/jetpackcomposelayouts/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.jetpackcomposelayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposelayouts.ui.theme.JetpackCompose_MVVMTheme
+
+class MainActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContent {
+            //注意这个 moudleNameTheme 名字好像不能换，多模块怎办呢？
+            JetpackCompose_MVVMTheme{
+                PhotographterCard()
+
+            }
+
+        }
+    }
+}
+
